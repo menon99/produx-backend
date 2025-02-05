bdba20e6b3b097f44a79e6944268255ec1e3cefc
Akash Menon <akashmenon11699cool@hotmail.com>
2025-02-06 02:01:06 +0530

    Fix connection

--- db.js
+++ db.js
@@ -1,16 +1,14 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const username = process.env.MONGO_USER;
-const password = process.env.MONGO_PASS;
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
 const dbName = process.env.MONGO_DB;
 
-const uri = `mongodb+srv://${username}:${password}@cluster0.ne5ud.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${username}:${password}@cluster0.ne5ud.mongodb.net/${dbName}/?retryWrites=true&w=majority`;
+console.log(uri);
 
 mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(uri)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
